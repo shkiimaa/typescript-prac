040b31751791798ff5dfea2704f448a0cbd8fc34
shkiimaa <shkiimaa@gmail.com>
2023-06-27 22:56:51 +0900

    chap 8 완료

---
+++ 8-exception/8-2-handling.ts
@@ -0,0 +1,43 @@
+{
+  class TimeoutError extends Error {}
+
+  class OfflineError extends Error {}
+
+  class NetworkClient {
+    tryConnect(): void {
+      throw new OfflineError(`No network!`);
+    }
+  }
+
+  class UserService {
+    constructor(private client: NetworkClient) {}
+
+    login() {
+      this.client.tryConnect();
+      //login....
+    }
+  }
+
+  class App {
+    constructor(private userServie: UserService) {}
+
+    run() {
+      try {
+        this.userServie.login();
+      } catch (error) {
+        /* catch로 받아오는 에러는 any 타입으로 
+        if(error instanceof OfflineError){} 를 사용하여 구분이 불가하다.
+        이럴땐 Error State를 사용하는 것이 좋다.
+        8-3으로 
+      */
+        console.log(`catched!`);
+      }
+    }
+  }
+
+  const client = new NetworkClient();
+  const service = new UserService(client);
+  const app = new App(service);
+
+  app.run();
+}
